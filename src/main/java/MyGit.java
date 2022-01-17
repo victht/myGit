7f284f9fb79534b79f5877874ad84afe39334145
layne <layne@atguigu.com>
2022-01-17 17:07:21 +0800

    hot-fix commit6

--- src/main/java/MyGit.java
+++ src/main/java/MyGit.java
@@ -2,5 +2,6 @@ public class MyGit {
     public static void main(String[] args) {
         System.out.println("testGit");
         System.out.println("testGit2");
+        System.out.println("hot-fix test");
     }
 }

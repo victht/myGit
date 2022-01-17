08894a15b5c8826a3ebd1d1f32e1d6b3d3c20975
layne <layne@atguigu.com>
2022-01-17 16:41:46 +0800

    second commit

--- src/main/java/MyGit.java
+++ src/main/java/MyGit.java
@@ -1,5 +1,7 @@
 public class MyGit {
     public static void main(String[] args) {
         System.out.println("testGit");
+        System.out.println("testGit2");
+        System.out.println("testGit3");
     }
 }

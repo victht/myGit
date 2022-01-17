637a9d1e123b44bcf2a2ba33e1bc9d2fb7bcfa0f
layne <layne@atguigu.com>
2022-01-17 17:05:57 +0800

    master commit5

--- src/main/java/MyGit.java
+++ src/main/java/MyGit.java
@@ -1,6 +1,6 @@
 public class MyGit {
     public static void main(String[] args) {
         System.out.println("testGit");
-        System.out.println("test1");
+        System.out.println("testGit2");
     }
 }
